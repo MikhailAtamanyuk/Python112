3ecbed7a8455af7b293a9a9e5095a91ee8280c73
MikhailAtamanyuk <atamanmisha250595@gmail.com>
2022-06-26 17:57:21 +0500

    112

--- 112.py
+++ 112.py
@@ -348,19 +348,44 @@ if __name__ == "__main__":
 import sqlite3 as sq
 
 
-with sq.connect('cars.db') as con:
-    cur = con.cursor()
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS cars(
-        car_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        model TEXT,
-        price INTEGER
-    )
-    """)
+cars = [
+    ('BMW', 54000),
+    ('Chevrolet', 46000),
+    ('Daewoo', 38000),
+    ('Citroen', 29000),
+    ('Honda', 33000),
+]
 
-    cur.execute("")
+
+# with sq.connect('cars.db') as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     CREATE TABLE IF NOT EXISTS cars(
+#         car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         model TEXT,
+#         price INTEGER
+#     )
+#     """)
+#
+#     cur.executescript("""
+#     DELETE FROM cars WHERE model LIKE 'B%';
+#     UPDATE cars SET price = price + 100;
+#     """)
+
+    # cur.execute("UPDATE cars SET price = :Price WHERE model LIKE 'B%'", {'Price': 0})
+
+    # cur.executemany("INSERT INTO cars VALUES(NULL, ?, ?)", cars)
+
+    # for car in cars:
+    #     cur.execute("INSERT INTO cars VALUES(NULL, ?, ?)", car)
+
+    # cur.execute("INSERT INTO cars VALUES(1, 'Renault', 22000)")
+    # cur.execute("INSERT INTO cars VALUES(2, 'Volvo', 29000)")
+    # cur.execute("INSERT INTO cars VALUES(3, 'Mercedes', 57000)")
+    # cur.execute("INSERT INTO cars VALUES(4, 'Bentley', 35000)")
+    # cur.execute("INSERT INTO cars VALUES(5, 'Audi', 52000)")
 
 # con.commit()
 # con.closer()
 
-
+# _______________________________________________________________________________________________
