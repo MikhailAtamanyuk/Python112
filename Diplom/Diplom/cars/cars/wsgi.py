af2e49ccd183b8f4bc04018b14def56be3689663
MikhailAtamanyuk <atamanmisha250595@gmail.com>
2022-09-23 17:14:20 +0500

    Diplom

--- Diplom/Diplom/cars/cars/wsgi.py
+++ Diplom/Diplom/cars/cars/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for personal_account project.
+WSGI config for cars project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_account.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cars.settings')
 
 application = get_wsgi_application()
